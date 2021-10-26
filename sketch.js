9b7b29789c5cab44eca03643d74946d9635eed5c
AntoGP <92826475+AntoGP@users.noreply.github.com>
2021-10-26 14:49:24 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  var canvas=createCanvas(640, 480);
+  //canvas.parent('lienzo');
+  background(233,181,247);
+}
+
+function draw() {
+  
+  fill(255,255,255)
+  textSize(40)
+  text("OSU!",random(640),random(480));
+}
+function mouseClicked(){
+  background(233,181,247);
+}
